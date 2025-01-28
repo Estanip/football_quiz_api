a22071547aecfb9c4f525eeed6f9b6e9ff43a1c9
estanip <estani.pettigrew@gmail.com>
2025-01-28 15:35:37 -0300

    feat: set score module

---
+++ src/modules/score/entities/score_history.entity.ts
@@ -0,0 +1,25 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { TypeOrmEntity } from 'src/modules/shared/entities/base-entity';
+import { UserEntity } from 'src/modules/user/entities/user.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+
+@Entity({ name: 'score_history' })
+@ObjectType({ description: 'Score History' })
+export class ScoreHistoryEntity extends TypeOrmEntity {
+  @ManyToOne(() => UserEntity, (user) => user.scoreHistory, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  @Field(() => UserEntity)
+  user: UserEntity;
+
+  @Column({ type: 'int' })
+  @Field(() => Int)
+  change: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  @Field({ nullable: true })
+  reason: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @Field()
+  changedAt: Date;
+}
