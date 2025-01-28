219d949ba4815ec30880335103a801d2a6cc1664
estanip <estani.pettigrew@gmail.com>
2025-01-28 15:37:55 -0300

    feat: set strategies

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtCookieStrategy extends PassportStrategy(Strategy, 'jwt-cookie') {
+  constructor() {
+    super({
+      jwtFromRequest: (req: Request) => {
+        if (req && req.cookies) return req.cookies['access_token'];
+        return null;
+      },
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.id, email: payload.email, role: payload.role };
+  }
+}
---
+++ src/modules/auth/strategies/local.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from 'src/modules/auth/auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private _authService: AuthService) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(email: string, password: string): Promise<any> {
+    const user = await this._authService.validateUser(email, password);
+    if (user) return { id: user.id, email: user.email, role: user.role };
+    else return null;
+  }
+}
