05da96e7e58c4ef8dfd3c1d1cc3f701ad36ead8b
estanip <estani.pettigrew@gmail.com>
2025-01-28 15:31:08 -0300

    feat: set env config & validator

---
+++ src/configuration/environment/env.config.ts
@@ -0,0 +1,84 @@
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+
+export const loadEnvironment = () => {
+  const envFile =
+    process.env.NODE_ENV === 'test' ? join(process.cwd(), 'test.env') : join(process.cwd(), '.env');
+  return dotenv.config({ path: envFile });
+};
+
+export type Environment = {
+  NODE_ENV: string;
+  DB_TYPE: string;
+  DB_HOST: string;
+  DB_PORT: string;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_NAME: string;
+  DB_SYNCHRONIZE: string;
+  CACHE_HOST: string;
+  CACHE_PORT: string;
+  CSRF_SECRET: string;
+  CSRF_COOKIE_TOKEN_NAME: string;
+  IS_PUBLIC_KEY: string;
+  JWT_SECRET: string;
+  NO_CSRF_KEY: string;
+  PORT: string;
+  ROLES_KEY: string;
+};
+
+interface EnvVariables {
+  database: {
+    type: string;
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    name: string;
+    synchronize: boolean;
+  };
+  cache: {
+    host: string;
+    port: number;
+  };
+  csrf: { cookie_token_name: string; secret: string };
+
+  env: string;
+  jwt: {
+    secret: string;
+  };
+  keys: { is_public: string; no_csrf: string; roles: string };
+  port: number;
+}
+
+const env = process.env as Environment;
+
+export default (): EnvVariables => ({
+  database: {
+    type: env.DB_TYPE,
+    host: env.DB_HOST,
+    port: parseInt(env.DB_PORT, 10) || 5432,
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    name: env.DB_NAME,
+    synchronize: env.DB_SYNCHRONIZE === 'true',
+  },
+  cache: {
+    host: env.CACHE_HOST,
+    port: parseInt(env.CACHE_PORT, 10),
+  },
+  csrf: {
+    cookie_token_name: env.CSRF_COOKIE_TOKEN_NAME,
+    secret: env.CSRF_SECRET,
+  },
+  env: env.NODE_ENV,
+  jwt: {
+    secret: env.JWT_SECRET,
+  },
+  keys: {
+    is_public: env.IS_PUBLIC_KEY,
+    no_csrf: env.NO_CSRF_KEY,
+    roles: env.ROLES_KEY,
+  },
+  port: parseInt(env.PORT, 10) || 3000,
+});
---
+++ src/configuration/environment/validation.schema.ts
@@ -0,0 +1,19 @@
+import * as Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+  DB_TYPE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required().default(5432),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  DB_SYNCHRONIZE: Joi.boolean().default(false),
+  CACHE_HOST: Joi.string().required(),
+  CACHE_PORT: Joi.number().required().default(6379),
+  IS_PUBLIC_KEY: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  NO_CSRF_KEY: Joi.string().required(),
+  PORT: Joi.number().required().default(3000),
+  ROLES_KEY: Joi.string().required(),
+});
