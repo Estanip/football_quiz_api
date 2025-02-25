cd4c9af7a710add84a7610225bef2478b9a0b9fd
estanip <estani.pettigrew@gmail.com>
2025-02-25 17:09:26 -0300

    feat: set morgan

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
+import * as morgan from 'morgan';
 import { HttpExceptionFilter } from 'src/common/exceptions/http.exception';
 import { ErrorInterceptor } from 'src/common/interceptors/error.interceptor';
 import { CsrfMiddleware } from 'src/common/middlewares/csfr.middleware';
@@ -13,21 +15,22 @@ async function bootstrap() {
 
   configureSwagger(app);
 
-  // TODO: improve CORS settings
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: '*',
-  });
-
   app.useGlobalInterceptors(new ErrorInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
   );
+  app.use(express.json());
   app.use(cookieParser());
   app.use(new CsrfMiddleware().use);
+  app.use(morgan('dev'));
+
+  app.enableCors({
+    origin: process.env.CLIENT_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'x-csrf-token'],
+    credentials: true,
+  });
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
