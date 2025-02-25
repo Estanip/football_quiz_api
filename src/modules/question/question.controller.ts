6c8b53af1fc2f6ba8c1dcf607b0a493ab7f67349
estanip <estani.pettigrew@gmail.com>
2025-02-25 17:16:22 -0300

    feat: add get question by subcategory & level controller

--- src/modules/question/question.controller.ts
+++ src/modules/question/question.controller.ts
@@ -1,9 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Req } from '@nestjs/common';
+import { Level } from 'src/constants/level';
 import { CreateQuestionDto } from 'src/modules/question/dto/create-question.dto';
 import { GetQuestionDto } from 'src/modules/question/dto/get-question.dto';
 import { QuestionEntity } from 'src/modules/question/entities/question.entity';
 import { QuestionService } from 'src/modules/question/question.service';
-import { BaseController } from 'src/modules/shared/controllers/base-controller';
+import { BaseController } from 'src/modules/shared/controllers/base.controller';
 import { ResponseService } from 'src/modules/shared/services/success-response.service';
 
 @Controller('question')
@@ -17,5 +18,24 @@ export class QuestionController extends BaseController<
     private readonly _responseService: ResponseService,
   ) {
     super(_questionService, _responseService, CreateQuestionDto, GetQuestionDto);
+  }
+
+  @Get('/:subcategory/:level')
+  async getQuestionsBySubcategoryAndLevel(
+    @Req() req,
+    @Param('subcategory') subcategory: number,
+    @Param('level') level: Level,
+  ) {
+    const questions = await this._questionService.findForUserBySubcategoryAndLevel(
+      req.user.id,
+      subcategory,
+      level,
+    );
+    return this._responseService.success(
+      true,
+      HttpStatus.FOUND,
+      'Questions successfully found',
+      questions,
+    );
   }
 }
