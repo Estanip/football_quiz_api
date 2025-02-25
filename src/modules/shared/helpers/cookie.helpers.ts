17eaf33f4b2e5214562306df04867217fa1a9e38
estanip <estani.pettigrew@gmail.com>
2025-02-25 17:02:18 -0300

    refactor: cookie set options helper

--- src/modules/shared/helpers/cookie.helpers.ts
+++ src/modules/shared/helpers/cookie.helpers.ts
@@ -2,14 +2,14 @@ import { SameSiteType } from 'csrf-csrf';
 import { CookieOptions } from 'express';
 
 export function _setCookiesOptions(
-  httpOnly: boolean,
-  minutes: number,
-  sameSite: SameSiteType = 'strict',
+  days: number = 1,
+  httpOnly: boolean = true,
+  sameSite: SameSiteType = 'lax',
 ): CookieOptions {
   return {
     httpOnly,
     secure: process.env.NODE_ENV === 'production',
-    maxAge: minutes * 60 * 1000,
+    maxAge: days * 24 * 60 * 60 * 1000,
     path: '/',
     sameSite,
   };
