5335319dcd301aa8083437d70e0ce7c94593f65d
estanip <estani.pettigrew@gmail.com>
2025-02-25 17:13:43 -0300

    feat: improve success response dto

--- src/modules/shared/dto/success-response.dto.ts
+++ src/modules/shared/dto/success-response.dto.ts
@@ -1,16 +1,22 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { ApiProperty } from '@nestjs/swagger';
 
+@ObjectType({ description: 'Success response' })
 export class SuccessResponseDto<T = any> {
   @ApiProperty({ example: true })
+  @Field(() => Boolean)
   success: boolean;
 
   @ApiProperty({ example: 200 })
+  @Field(() => Int)
   statusCode: number;
 
   @ApiProperty({ example: 'Operation completed successfully' })
+  @Field(() => String)
   message: string;
 
   @ApiProperty({ example: {} })
+  @Field(() => [], { nullable: true })
   data: T;
 
   constructor(success: boolean, statusCode: number, message: string, data: T = {} as T) {
