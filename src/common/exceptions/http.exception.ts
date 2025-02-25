f4def163f9cb6b2d8f38db3a5ed150161e2bfb0d
estanip <estani.pettigrew@gmail.com>
2025-02-25 17:05:20 -0300

    feat: improve http exception

--- src/common/exceptions/http.exception.ts
+++ src/common/exceptions/http.exception.ts
@@ -1,5 +1,7 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { GqlArgumentsHost } from '@nestjs/graphql';
 import { Request, Response } from 'express';
+import { HostType, HostTypes } from 'src/common/constants/host_types.constants';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
@@ -9,13 +11,29 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest<Request>();
     const status = exception?.getStatus();
     const exceptionError = exception?.getResponse() as { message: string; error: string };
+    const hostType = host.getType() as HostType;
 
-    response.status(status).json({
-      error: exceptionError?.error || exceptionError,
-      statusCode: status,
-      message: exceptionError?.message || exception?.message,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
+    if (hostType === HostTypes.GRAPHQL) {
+      const gqlHost = GqlArgumentsHost.create(host);
+      const path = gqlHost.getInfo().path.key;
+
+      throw new HttpException(
+        {
+          error: exceptionError?.error || exceptionError,
+          statusCode: status,
+          message: exceptionError?.message || exception?.message,
+          timestamp: new Date().toISOString(),
+          path: `graphql/${path}`,
+        },
+        status,
+      );
+    } else
+      response.status(status).json({
+        error: exceptionError?.error || exceptionError,
+        statusCode: status,
+        message: exceptionError?.message || exception?.message,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
   }
 }
