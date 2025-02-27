71b8ba590f4a002394512a95b66806c1d3061107
estanip <estani.pettigrew@gmail.com>
2025-02-27 15:49:18 -0300

    feat: add a welcome controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { Throttle, ThrottlerGuard } from '@nestjs/throttler';
 import { AppService } from 'src/app.service';
 import { IsPublic } from 'src/modules/shared/decorators/isPublic.decorator';
@@ -6,6 +6,16 @@ import { IsPublic } from 'src/modules/shared/decorators/isPublic.decorator';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @IsPublic()
+  @Get()
+  @UseGuards(ThrottlerGuard)
+  @Throttle({ default: { limit: 60, ttl: 60000 } })
+  get(@Req() req: Request, @Res() res) {
+    return res.json({
+      message: `Welcome to Futbol Quiz API, go to http://${req?.headers['host']}/api to see available endpoints`,
+    });
+  }
 
   @IsPublic()
   @Get('health')
